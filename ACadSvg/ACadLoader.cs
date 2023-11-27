97396d383783a28130121610ffad94f6e0c30df2
Dr. Matthias Menningen <matthias.menningen@nanologika.de>
2023-11-27 17:04:44 +0100

    #37 Fixed -  new method ACadLoader.LoadDXF

--- ACadSvg/ACadLoader.cs
+++ ACadSvg/ACadLoader.cs
@@ -22,11 +22,35 @@ namespace ACadSvg {
     /// </remarks>
     public static class ACadLoader {
 
+        /// <summary>
+        /// Loads a DWG file from the specified path and converts it to a SVG/XML structure.
+        /// SVG is represented by a <see cref="DocumentSvg"/> object.
+        /// </summary>
+        /// <param name="path">The path of the DWG file to be read.</param>
+        /// <param name="ctx">The conversion context providing conversion options.
+        /// It also received the conversion log.</param>
+        /// <returns>A <see cref="DocumentSvg" /> representing the read document converted to SVG/XML.</returns>
         public static DocumentSvg LoadDwg(string path, ConversionContext ctx) {
             ctx.ConversionInfo.Log($"Loading DWG from \"{path}\" started");
             CadDocument doc = DwgReader.Read(path);
 			DocumentSvg docSvg = new DocumentSvg(doc, ctx);
 			return docSvg;
 		}
-    }
+
+
+        /// <summary>
+        /// Loads a DXF file from the specified path and converts it to a SVG/XML structure.
+        /// SVG is represented by a <see cref="DocumentSvg"/> object.
+        /// </summary>
+        /// <param name="path">The path of the DWG file to be read.</param>
+        /// <param name="ctx">The conversion context providing conversion options.
+        /// It also received the conversion log.</param>
+        /// <returns>A <see cref="DocumentSvg" /> representing the read dicument converted to SVG/XML.</returns>
+		public static DocumentSvg LoadDxf(string path, ConversionContext ctx) {
+			ctx.ConversionInfo.Log($"Loading DXF from \"{path}\" started");
+			CadDocument doc = DxfReader.Read(path);
+			DocumentSvg docSvg = new DocumentSvg(doc, ctx);
+			return docSvg;
+		}
+	}
 }
