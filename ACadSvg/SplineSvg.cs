61857940f6ae79cf482fdb5769abdeb10dcbb585
Dr. Matthias Menningen <matthias.menningen@nanologika.de>
2023-12-05 14:03:07 +0100

    -  formatting

--- ACadSvg/SplineSvg.cs
+++ ACadSvg/SplineSvg.cs
@@ -6,14 +6,12 @@
 #endregion
 
 using ACadSharp.Entities;
-using ACadSvg.SplineUtils;
 using CSMath;
-
 using SvgElements;
+using ACadSvg.SplineUtils;
 
 
-namespace ACadSvg
-{
+namespace ACadSvg {
 
     /// <summary>
     /// Represents an SVG element converted from an ACad <see cref="Spline"/> entity.
@@ -26,33 +24,33 @@ namespace ACadSvg
         private Spline _spline;
 
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="SplineSvg"/> class
-		/// for the specified <see cref="Spline"/> entity.
-		/// </summary>
-		/// <param name="spline">The <see cref="Spline"/> entity to be converted.</param>
-		/// <param name="ctx">This parameter is not used in this class.</param>
-		public SplineSvg(Entity spline, ConversionContext ctx) : base(ctx) {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SplineSvg"/> class
+        /// for the specified <see cref="Spline"/> entity.
+        /// </summary>
+        /// <param name="spline">The <see cref="Spline"/> entity to be converted.</param>
+        /// <param name="ctx">This parameter is not used in this class.</param>
+        public SplineSvg(Entity spline, ConversionContext ctx) : base(ctx) {
             _spline = (Spline)spline;
-			SetStandardIdAndClassIf(spline, ctx);
-		}
+            SetStandardIdAndClassIf(spline, ctx);
+        }
 
 
-		/// <inheritdoc />
-		public override SvgElementBase ToSvgElement() {
+        /// <inheritdoc />
+        public override SvgElementBase ToSvgElement() {
 
-			if (_spline.ControlPoints.Count > 0) {
-				switch (_spline.ControlPoints.Count) {
-				case 3:
-					return new PathElement()
-						.AddMoveAndQSpline(Utils.VerticesToArray(_spline.ControlPoints))
+            if (_spline.ControlPoints.Count > 0) {
+                switch (_spline.ControlPoints.Count) {
+                case 3:
+                    return new PathElement()
+                        .AddMoveAndQSpline(Utils.VerticesToArray(_spline.ControlPoints))
                         .WithID(ID)
                         .WithClass(Class)
                         .WithStroke(ColorUtils.GetHtmlColor(_spline, _spline.Color))
-	                    .WithStrokeWidth(LineUtils.GetLineWeight(_spline.LineWeight, _spline, _ctx))
+                        .WithStrokeWidth(LineUtils.GetLineWeight(_spline.LineWeight, _spline, _ctx))
                         .WithStrokeDashArray(LineUtils.LineToDashArray(_spline, _spline.LineType));
 
-				case 4:
+                case 4:
                     return new PathElement()
                         .AddMoveAndCSpline(Utils.VerticesToArray(_spline.ControlPoints))
                         .WithID(ID)
@@ -60,9 +58,9 @@ namespace ACadSvg
                         .WithStroke(ColorUtils.GetHtmlColor(_spline, _spline.Color))
                         .WithStrokeWidth(LineUtils.GetLineWeight(_spline.LineWeight, _spline, _ctx))
                         .WithStrokeDashArray(LineUtils.LineToDashArray(_spline, _spline.LineType));
-						
-				default:
-					IList<XY> points = NURBS.CreateBSplineCurve(_spline.Degree, _spline.ControlPoints, _spline.Knots);
+
+                default:
+                    IList<XY> points = NURBS.CreateBSplineCurve(_spline.Degree, _spline.ControlPoints, _spline.Knots);
 
                     var pathElement = new PathElement()
                         .AddPoints(Utils.VerticesToArray(points))
@@ -73,16 +71,16 @@ namespace ACadSvg
                         .WithStrokeDashArray(LineUtils.LineToDashArray(_spline, _spline.LineType));
 
                     return pathElement;
-     
+
                     //GroupElement g = new GroupElement();
                     //g.Children.Add(pathElement);
                     //foreach (var point in _spline.ControlPoints) {
                     //    g.Children.Add(new CircleElement() { Cx = point.X, Cy = point.Y, R = 0.5 }.WithStroke("red"));
                     //}
-					//return g;
+                    //return g;
                 }
             }
-			else if (_spline.FitPoints != null) {
+            else if (_spline.FitPoints != null) {
                 Utils.XYZToDoubles(_spline.FitPoints, out double[] xs, out double[] ys);
                 getTangentDxDy(_spline.StartTangent, out double firstDx, out double firstDy);
                 getTangentDxDy(_spline.EndTangent, out double lastDx, out double lastDy);
@@ -104,7 +102,7 @@ namespace ACadSvg
             }
 
             return null;
-		}
+        }
 
 
         private void getTangentDxDy(XYZ et, out double dx, out double dy) {
